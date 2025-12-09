7c61f33139525b35a2f9ed2d44317ad16af42941
shahriar0999 <shahriar0999@proton.me>
2025-12-09 10:37:48 +0600

    calculations added

---
+++ app/calculations.py
@@ -0,0 +1,13 @@
+def add(num1: float, num2: float) -> float:
+    return num1 + num2
+
+def subtract(num1: float, num2: float) -> float:
+    return num1 - num2
+
+def multiply(num1: float, num2: float) -> float:
+    return num1 * num2
+
+def divide(num1: float, num2: float) -> float:
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero.")
+    return num1 / num2
