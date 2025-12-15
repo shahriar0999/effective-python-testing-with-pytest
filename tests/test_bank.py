9ede78c1ddfb94d2a56292696589d9ed625cf251
shahriar0999 <shahriar0999@proton.me>
2025-12-15 06:22:08 +0600

    use pytest fixture to reduce repetitive task

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,27 +1,31 @@
 from app.bank import BankAccount
+import pytest 
 
 
-def test_default_balance():
-    account = BankAccount()
-    assert account.get_balance() == 0
+@pytest.fixture
+def default_bank_account():
+    return BankAccount()
 
-def test_initial_balance():
-    account = BankAccount(initial_balance=100)
-    assert account.get_balance() == 100
+@pytest.fixture
+def bank_account():
+    return BankAccount(initial_balance=200)
 
-def test_deposit():
-    account = BankAccount()
-    account.deposit(50)
-    assert account.get_balance() == 50
+def test_default_balance(default_bank_account):
+    assert default_bank_account.get_balance() == 0
 
-def test_withdraw():
-    account = BankAccount(initial_balance=100)
-    account.withdraw(30)
-    assert account.get_balance() == 70
+def test_initial_balance(bank_account):
+    assert bank_account.get_balance() == 200
 
-def test_withdraw_insufficient_funds():
-    account = BankAccount(initial_balance=50)
+def test_deposit(bank_account):
+    bank_account.deposit(50)
+    assert bank_account.get_balance() == 250
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(30)
+    assert bank_account.get_balance() == 170
+
+def test_withdraw_insufficient_funds(bank_account):
     try:
-        account.withdraw(100)
+        bank_account.withdraw(100)
     except ValueError as e:
         assert str(e) == "Insufficient funds"
